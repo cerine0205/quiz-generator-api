9509201c5c3cd6e0aca6a8b522d8e908cc2cacac
cerine <saljahdali0205@outlook.com>
2026-04-25 11:57:18 +0300

    feat: add Chat and Message models with migrations and relationships

---
+++ app/Models/Message.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    protected $fillable = [
+        'chat_id',
+        'sender',
+        'type',
+        'content'
+    ];
+
+    public function chat()
+    {
+        return $this->belongsTo(Chat::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function chats()
+    {
+        return $this->hasMany(Chat::class);
+    }
 }
